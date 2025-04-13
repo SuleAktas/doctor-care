11158a28bc407ab38882c7e5288fec552a11b34b
SuleAktas <suleaktas@live.nl>
2025-04-13 13:25:38 +0200

    chore: fix folder names

---
+++ src/components/Solution/Solution.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Solution.css";
+
+function Solution(props) {
+  return (
+    <div className="solution-box">
+      <div className="solution-check">
+        <div className="solution-check-mark"></div>
+      </div>
+      <div className="solution-title">{props.title}</div>
+      <div className="solution-text">{props.text}</div>
+    </div>
+  );
+}
+
+export default Solution;

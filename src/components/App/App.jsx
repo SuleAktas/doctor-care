7b5d417240b2022ae7434625b952fbe24baccc9c
SuleAktas <suleaktas@live.nl>
2025-05-05 20:27:32 +0200

    fix:code refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,15 +10,6 @@ import Achievements from '../Achievements/Achievements';
 function App() {
   const { t } = useTranslation();
 
-  const solutions = [
-    { title: t('solution1Title'), text: t('solution1Exp') },
-    { title: t('solution2Title'), text: t('solution2Exp') },
-    { title: t('solution3Title'), text: t('solution3Exp') },
-    { title: t('solution4Title'), text: t('solution4Exp') },
-    { title: t('solution5Title'), text: t('solution5Exp') },
-    { title: t('solution6Title'), text: t('solution6Exp') },
-  ];
-
   return (
     <>
       <Header />
@@ -31,13 +22,16 @@ function App() {
           <div className="solution-main-title">{t('solutonMainTitle')}</div>
         </div>
         <div className="solution-boxes">
-          {solutions.map((solution, index) => (
-            <Solution key={index} title={solution.title} text={solution.text} />
+          {[...new Array(6)].map((_, index) => (
+            <Solution
+              key={index}
+              title={t(`solution${index + 1}Title`)}
+              text={t(`solution${index + 1}Exp`)}
+            />
           ))}
         </div>
       </div>
       <About />
-      <img />
       <Contact />
       <Footer />
     </>
